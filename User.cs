876c3477aea6aed47695fe00c7e8fa64d232f18e
MukheshRavi <mukeshparvathi@gmail.com>
2020-10-06 23:57:22 +0530

    sample mails validation added

--- User.cs
+++ User.cs
@@ -86,6 +86,20 @@ namespace UserRegistration
 
         }
 
+        public void ValidateSampleEmail()
+        {
+            string patternOfEmail = "^[a-zA-Z0-9]+([.+_-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})?$";
+            string[] sampleEmail = {"abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au",
+                                    "abc@1.com","abc@gmail.com.com","abc+100@gmail.com","Abc","abc@.com.my" ,"abc123@gmail.1a","abc123@.com","abc123@.com.com",".abc@abc.com","abc()*@gmail.com","abc@%*.com","abc..2002@gmail.com","abc.@gmail.com","abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au"};
+            Console.WriteLine("\n\nThe valid sample emails are :");
+            foreach (string sample in sampleEmail)
+            {
+                if (Regex.IsMatch(sample, patternOfEmail))
+                    Console.WriteLine(sample);
+
+            }
+        }
+
 
 
 
