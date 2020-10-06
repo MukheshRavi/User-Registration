0291af486196ed1ccb35c23fb9e6e2d994a440fa
MukheshRavi <mukeshparvathi@gmail.com>
2020-10-06 22:28:20 +0530

    uc4 added

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace UserRegistration
             user.validateFrstName();
             user.validateLastName();
             user.validateEmail();
+            user.validatePhoneNumber();
 
 
 
