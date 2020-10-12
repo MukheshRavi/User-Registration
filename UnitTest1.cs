d3074651fae43b38e363d726be608c3008f17324
MukheshRavi <mukeshparvathi@gmail.com>
2020-10-12 13:31:02 +0530

    [Mukesh].Refactor Ms Testing for multiple emails method

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -45,8 +45,25 @@ namespace UserMsTest
             //Assert
             Assert.AreEqual(expected, actual);
         }
-
-
+        [DataTestMethod]
+        [DataRow(true, "abc@yahoo.com")]
+        [DataRow(true, "abc-100@yahoo.com")]
+        [DataRow(true, "abc.100@yahoo.com")]
+        [DataRow(true, "abc111@abc.com")]
+        [DataRow(true, "abc-100@abc.net")]
+        [DataRow(true, "abc+100@gmail.com")]
+        [DataRow(true, "abc123@gmail.1a")]
+        [DataRow(true, "abc@1.com")]
+        [DataRow(true, "abc@.com.my")]
+        [DataRow(true, "abc123@.com.com")]
+        public void TestMultipleEmails(bool expected,string email)
+        {//Arrange
+           
+            //Act
+            bool actual = User.ValidateEmail(email);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
 
 
     }
